266fae2b5174b80ec5e9ae440f15822de8c4938f
programadormovel <programadormovel@gmail.com>
2025-03-12 11:06:35 -0300

    menu

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
-import '../node_modules/bootstrap//dist/js/bootstrap.js';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
 import './index.css'
 import AppRoutes from './components/AppRoutes/index.jsx';
 
--- src/templates/App/App.jsx
+++ src/templates/App/App.jsx
@@ -1,20 +1,55 @@
 import { useState } from 'react'
-	import './App.css'
-	import { Link } from 'react-router-dom'
+import './App.css'
+import { Link } from 'react-router-dom'
 
-	function App() {
-	  const [count, setCount] = useState(0)
+function App() {
+	const [count, setCount] = useState(0)
 
-	  return (
-	    <div className='container'>
-	      <div className="d-flex justify-content-center align-items-center" style={{height: '100vh'}}>
-	        <button className='btn btn-primary' onClick={() => setCount((count) => count + 1)}>
-	          <i className="bi bi-arrow-repeat"></i> count is {count} <i className="bi bi-trophy-fill"></i>
-        	</button>
-	        <Link to={'/home'} className='btn btn-warning' >Home</Link>
-	      </div>
-	    </div>
-	  )
-	}
+	return (
+		<div className='container'>
+			<nav class="navbar navbar-expand-lg navbar-light bg-light">
+				<a class="navbar-brand" href="#">Navbar</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
 
-	export default App
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav mr-auto">
+						<li class="nav-item active">
+							<Link to={'/home'} className='btn btn-warning' >Home</Link>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Link</a>
+						</li>
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+								Dropdown
+							</a>
+							<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+								<a class="dropdown-item" href="#">Action</a>
+								<a class="dropdown-item" href="#">Another action</a>
+								<div class="dropdown-divider"></div>
+								<a class="dropdown-item" href="#">Something else here</a>
+							</div>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link disabled" href="#">Disabled</a>
+						</li>
+					</ul>
+					<form class="form-inline my-2 my-lg-0">
+						{/* <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"> */}
+						<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+					</form>
+				</div>
+			</nav>
+			<div className="d-flex column justify-content-center align-items-start" style={{ height: '100vh', width: '100vh' }}>
+				<button className='btn btn-primary' onClick={() => setCount((count) => count + 1)}>
+					<i className="bi bi-arrow-repeat"></i> count is {count} <i className="bi bi-trophy-fill"></i>
+				</button>
+				
+			</div>
+		</div>
+	)
+}
+
+export default App
